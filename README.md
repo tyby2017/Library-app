eee161380f02d58c8fd26efed17cad63a32f9952
Tiberiu <tiberiu.miinea@gmail.com>
2021-03-31 10:45:41 +0300

    Redone most of the project and added delete functionality

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Library App
-
-Draft of the project. Didn't manage to add remove button for each book.
+# Library App Project
--- scripts.js
+++ scripts.js
@@ -1,83 +1,40 @@
 let Library = [];
-const emptyLibraryDiv = document.getElementById('library-div').innerHTML;
-let removeButtonsArray = [];
 
-//ADD
-const addButton = document.getElementById('add-book');
-addButton.addEventListener('click', makeFormVisible);
-
-function makeFormVisible() {
-    const submit = document.getElementById('submit');
-    const bookForm = document.getElementById('add-book-form');
-    if((submit.style.visibility = 'hidden') && (bookForm.style.visibility = 'hidden')) {
-        submit.style.visibility = 'visible';
-        bookForm.style.visibility  ='visible';
-    }
-}
-
-function makeFormInvisible() {
-    const submit = document.getElementById('submit');
-    const bookForm = document.getElementById('add-book-form');
-    if((submit.style.visibility = 'visible') && (bookForm.style.visibility = 'visible')) {
-        submit.style.visibility = 'hidden';
-        bookForm.style.visibility  ='hidden';
-    } 
-}
-
-//SUBMIT
-const submitButton = document.getElementById('submit');
-submitButton.addEventListener('click', () => {
-    makeFormInvisible();
-    resetLibraryDiv();
-    addBooks();
-    document.getElementById('add-book-form').reset();
-    addBooksToDiv();
-    
-
-
-});
-
-//BOOK CONSTRUCTOR
-function Book(title,author,pages) {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
 }
 
-//ADD BOOKS to LIBRARY ARRAY
-function addBooks() {
-    let book = new Book();
-    book.title = document.getElementById('title').value;
-    book.author = document.getElementById('author').value;
-    book.pages = document.getElementById('pages').value;
+function addToLibrary() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const book = new Book(title,author,pages);
     Library.push(book);
+}
+
+const myButton = document.querySelector('.btn-list');
+const list = document.querySelector('.container ul');
+myButton.addEventListener('click', (e)=>{
+    addToLibrary();
+    document.querySelector('form').reset();
+    let inputText = `${Library[Library.length-1].title} by ${Library[Library.length-1].author}, ${Library[Library.length-1].pages} pages`;
+    if(inputText != '')
+    {
+        e.preventDefault();
+        const myLi = document.createElement('li');
+        myLi.innerHTML = inputText;
+        list.appendChild(myLi);
+
+        const mySpan = document.createElement('span');
+        mySpan.innerHTML = 'x';
+        myLi.appendChild(mySpan);
     }
-
-//ADD BOOKS to DIV
-function addBooksToDiv() {
-    let counter = 0;    
-    Library.forEach(book => {        
-        const container = document.createElement('div');
-        container.setAttribute('class','container');
-        const libraryDiv = document.getElementById('library-div');
-        libraryDiv.appendChild(container);
-        //create div for book
-        const div = document.createElement('div');
-        div.innerHTML = `${book.title} by ${book.author}, ${book.pages} pages`;
-        div.setAttribute('class','book-div');
-        container.appendChild(div);
-        //create button for remove
-        const button = document.createElement('button');
-        button.innerHTML = '-';        
-        button.setAttribute('id', `${counter}`);
-        button.setAttribute('class','removeButton');        
-        container.appendChild(button);
-        counter++;
-    });    
-}
-
-function resetLibraryDiv() {
-    document.getElementById('library-div').innerHTML = emptyLibraryDiv;
-}
-
-//EVENT LISTENER FOR REMOVE BUTTONS
+    const close = [...document.querySelectorAll('span')];
+    close.forEach(button => {
+        button.addEventListener('click', ()=>{
+            button.parentElement.style.display = 'none';
+        });
+    });
+});
