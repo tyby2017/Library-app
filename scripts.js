6c301cd23111cf270ec39e3d99da3ab7f44defb1
Tiberiu <tiberiu.miinea@gmail.com>
2021-03-31 08:05:21 +0300

    Draft 2. Still cannot add remove buttons

--- scripts.js
+++ scripts.js
@@ -1,84 +1,83 @@
-let myLibrary = [];
-const emptyLib = document.getElementById('lib').innerHTML;
+let Library = [];
+const emptyLibraryDiv = document.getElementById('library-div').innerHTML;
+let removeButtonsArray = [];
 
-function Book(title,author, pages) {
+//ADD
+const addButton = document.getElementById('add-book');
+addButton.addEventListener('click', makeFormVisible);
+
+function makeFormVisible() {
+    const submit = document.getElementById('submit');
+    const bookForm = document.getElementById('add-book-form');
+    if((submit.style.visibility = 'hidden') && (bookForm.style.visibility = 'hidden')) {
+        submit.style.visibility = 'visible';
+        bookForm.style.visibility  ='visible';
+    }
+}
+
+function makeFormInvisible() {
+    const submit = document.getElementById('submit');
+    const bookForm = document.getElementById('add-book-form');
+    if((submit.style.visibility = 'visible') && (bookForm.style.visibility = 'visible')) {
+        submit.style.visibility = 'hidden';
+        bookForm.style.visibility  ='hidden';
+    } 
+}
+
+//SUBMIT
+const submitButton = document.getElementById('submit');
+submitButton.addEventListener('click', () => {
+    makeFormInvisible();
+    resetLibraryDiv();
+    addBooks();
+    document.getElementById('add-book-form').reset();
+    addBooksToDiv();
+    
+
+
+});
+
+//BOOK CONSTRUCTOR
+function Book(title,author,pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
 }
 
-function addBookToLibrary() {
-    const book = new Book();
+//ADD BOOKS to LIBRARY ARRAY
+function addBooks() {
+    let book = new Book();
     book.title = document.getElementById('title').value;
     book.author = document.getElementById('author').value;
-    book.pages = parseInt(document.getElementById('pages').value);
-    myLibrary.push(book);
-}
+    book.pages = document.getElementById('pages').value;
+    Library.push(book);
+    }
 
-function loopMyLibrary() { 
-    myLibrary.forEach(book => {
+//ADD BOOKS to DIV
+function addBooksToDiv() {
+    let counter = 0;    
+    Library.forEach(book => {        
+        const container = document.createElement('div');
+        container.setAttribute('class','container');
+        const libraryDiv = document.getElementById('library-div');
+        libraryDiv.appendChild(container);
+        //create div for book
         const div = document.createElement('div');
+        div.innerHTML = `${book.title} by ${book.author}, ${book.pages} pages`;
         div.setAttribute('class','book-div');
-        const deleteButton = document.createElement('button');
-        deleteButton.innerHTML = '-';
-        deleteButton.setAttribute('class', 'remove-btn');
-        div.innerHTML = `${book.title} by ${book.author}, ${book.pages} pages`;        
-        div.appendChild(deleteButton);
-        document.getElementById('lib').appendChild(div);
-    });
+        container.appendChild(div);
+        //create button for remove
+        const button = document.createElement('button');
+        button.innerHTML = '-';        
+        button.setAttribute('id', `${counter}`);
+        button.setAttribute('class','removeButton');        
+        container.appendChild(button);
+        counter++;
+    });    
 }
 
-function makeFormVisible() {
-    const form = document.getElementById('form');
-    const submit = document.getElementById('submit');
-        form.style.visibility = 'visible';
-        submit.style.visibility = 'visible';
+function resetLibraryDiv() {
+    document.getElementById('library-div').innerHTML = emptyLibraryDiv;
 }
 
-function makeFormInvisible() {
-    const form = document.getElementById('form');
-    const submit = document.getElementById('submit');
-        form.style.visibility = 'hidden';
-        submit.style.visibility = 'hidden';
-}
-
-function resetForm() {
-    const inputs = Array.from(document.getElementsByTagName('input'));
-    inputs.forEach(input => {
-        input.setAttribute.value = '';     
-    }); 
-}
-
-function emptyLibrary() {
-    document.getElementById('lib').innerHTML = emptyLib;
-}
-
-//  front-end part
-const button = document.getElementById('add-book');
-button.addEventListener('click', makeFormVisible);
-
-const submit = document.getElementById('submit');
-submit.addEventListener('click', () => {
-    addBookToLibrary();
-    emptyLibrary();    
-    loopMyLibrary();
-    makeFormInvisible();
-    document.getElementById('form').reset();
-    removeBtns = [...document.getElementsByClassName('remove-btn')];
-    listenRmvButtons();
-});
-
-console.log(removeBtns);
-
-function listenRmvButtons() {
-    removeBtns.forEach(button => {
-        button.addEventListener('click', () => {
-            const index = button.getAttribute('id');
-            myLibrary.splice(index,1);
-        });
-    });
-}
-
-
-
-
+//EVENT LISTENER FOR REMOVE BUTTONS
